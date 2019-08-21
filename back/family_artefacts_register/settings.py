bdc74dcdeb5d67283b2dbcad1bcb85fcb2e08474
Kyle Zsembery <k.zsembery@student.unimelb.edu.au>
2019-08-21 12:48:05 +1000

    Some react/apollo and graphql integration

--- back/family_artefacts_register/settings.py
+++ back/family_artefacts_register/settings.py
@@ -53,14 +53,12 @@ GRAPHENE = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # Third party middleware
-    'corsheaders.middleware.CorsMiddleWare',
-    # Custom middleware
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
