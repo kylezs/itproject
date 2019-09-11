352b712a370d6d45c494e0e8a95207678c0040a2
Kyle Zsembery <k.zsembery@student.unimelb.edu.au>
2019-09-11 11:24:18 +1000

    Remove zane's venv from repo

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\zane_\documents\uni\year3\sem2\comp30022\family-artefacts-register\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
