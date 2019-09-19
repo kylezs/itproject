26c933e71c43e267bf3fc49563fb0a33e4ad04da
Kyle Zsembery <k.zsembery@student.unimelb.edu.au>
2019-09-19 15:47:49 +1000

    Logout route import fixed

--- front/src/App.js
+++ front/src/App.js
@@ -8,6 +8,7 @@ import CreateView from './views/CreateView'
 import DetailView from './views/DetailView'
 import Login from './views/LoginView'
 import Signup from './views/SignupView'
+import Logout from './components/Logout'
 // Family views
 import CreateFamilyView from './views/CreateFamilyView'
 
@@ -23,6 +24,7 @@ class App extends Component {
                         {/* User auth routes */}
                         <Route exact path="/login/" component={Login} />
                         <Route exact path="/signup/" component={Signup} />
+                        <Route exact path="/logout/" component={Logout} />
 
                         {/* Artefact routes */}
                         <PrivateRoute exact path="/artefacts/create/" component={CreateView} />
@@ -30,6 +32,7 @@ class App extends Component {
 
                         {/* Family routes */}
                         <PrivateRoute exact path="/family/create" component={CreateFamilyView} />
+                        
                     </Switch>
                 </div>
             </Router>
