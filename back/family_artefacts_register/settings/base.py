29f74590fbf442c8b5384e2767d1366273dda1c1
EthanMiao <zhuopingm@gmail.com>
2019-09-19 14:25:09 +1000

    AWS S3 setting ready to merge

--- back/family_artefacts_register/settings/base.py
+++ back/family_artefacts_register/settings/base.py
@@ -55,9 +55,6 @@ INSTALLED_APPS = [
     'artefacts',
     'userprofile',
     'family',
-
-    # Amazon AWS
-    'storages',
 ]
 
 GRAPHENE = {
@@ -154,23 +151,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Artefacts Picture (AWS S3)
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'family_artefacts_register/static'),
-]
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-AWS_ACCESS_KEY_ID = 'AKIARVLDMTEQV22SWEWB'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'artefact-picture'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-#STATIC_URL = '/static/'
-DEFAULT_FILE_STORAGE = 'family_artefacts_register.storage_backends.MediaStorage'
+STATIC_URL = '/static/'
