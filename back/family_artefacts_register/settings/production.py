bda16d7a57128d9669b7466e09d5adabe656e036
Kyle Zsembery <k.zsembery@student.unimelb.edu.au>
2019-09-25 16:43:47 +1000

    Update prod settings

--- back/family_artefacts_register/settings/production.py
+++ back/family_artefacts_register/settings/production.py
@@ -15,31 +15,29 @@ os.makedirs(STATIC_TMP, exist_ok=True)
 ALLOWED_HOSTS = ['https://family-artefacts-register.herokuapp.com',
                  "*.herokuapp.com", "127.0.0.1:8000"]
 
-
-
 INSTALLED_APPS += [
     # Amazon AWS
     'storages',
 ]
 
 # Artefacts Picture (AWS S3)
-"""
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'family_artefacts_register/static'),
-]
+# """
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'family_artefacts_register/static'),
+# ]
 
-AWS_ACCESS_KEY_ID = 'AKIARVLDMTEQV22SWEWB'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'artefact-picture'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_ACCESS_KEY_ID = 'AKIARVLDMTEQV22SWEWB'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'artefact-picture'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
 
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# AWS_LOCATION = 'static'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'family_artefacts_register.storage_backends.MediaStorage'
-"""
+# DEFAULT_FILE_STORAGE = 'family_artefacts_register.storage_backends.MediaStorage'
+# """
