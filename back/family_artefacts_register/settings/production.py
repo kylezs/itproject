f155b9a5868cbd05cc1c1aa697f0066d800912c0
Kyle Zsembery <k.zsembery@student.unimelb.edu.au>
2019-09-26 10:14:50 +1000

    Ready for production with AWS

--- back/family_artefacts_register/settings/production.py
+++ back/family_artefacts_register/settings/production.py
@@ -27,18 +27,17 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Artefacts Picture (AWS S3)
 
-# AWS_ACCESS_KEY_ID = 'AKIARVLDMTEQV22SWEWB'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'artefact-picture'
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_ACCESS_KEY_ID = 'AKIARVLDMTEQV22SWEWB'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'artefact-picture'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 
-# AWS_LOCATION = 'static'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-# DEFAULT_FILE_STORAGE = 'family_artefacts_register.storage_backends.MediaStorage'
-# """
+DEFAULT_FILE_STORAGE = 'family_artefacts_register.storage_backends.MediaStorage'
