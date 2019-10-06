5d2cae7a339551384bdb2acfce74e7ad0ef944ce
Kyle Zsembery <k.zsembery@student.unimelb.edu.au>
2019-10-06 12:49:51 +1100

    Add home link to 404

--- front/src/views/Error404View.js
+++ front/src/views/Error404View.js
@@ -6,6 +6,7 @@ export default function Error404View(props) {
             <h1 style={{color: 'red'}}>404 Page Not Found</h1>
             <h3>If you're seeing this page...</h3>
             <img src="https://media0.giphy.com/media/KVZXay4Uduy8XHpZRc/giphy.gif" />
+            <p><a href="/">Return home</a></p>
         </div>
     )
 }
