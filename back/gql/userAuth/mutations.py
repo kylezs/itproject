54761fd937190b476cc30cec86892467776bf3f3
Kyle Zsembery <k.zsembery@student.unimelb.edu.au>
2019-10-17 16:29:39 +1100

    King's firstname, lastname stuff for user creation

--- back/gql/userAuth/mutations.py
+++ back/gql/userAuth/mutations.py
@@ -8,15 +8,21 @@ from .types import UserType
 class CreateUser(graphene.Mutation):
     user = graphene.Field(UserType)
 
+    # username is the field that acts as a pseudo-primary key
     class Arguments:
         username = graphene.String(required=True)
         password = graphene.String(required=True)
         email = graphene.String(required=True)
+        firstname = graphene.String()
+        lastname = graphene.String()
 
-    def mutate(self, info, username, password, email):
+
+    def mutate(self, info, username, password, email, firstname, lastname):
         user = get_user_model()(
             username=username,
             email=email,
+            first_name=firstname,
+            last_name=lastname
         )
         user.set_password(password)
         user.save()
