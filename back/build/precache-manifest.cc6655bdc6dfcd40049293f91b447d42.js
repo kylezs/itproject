82c59268a9c16a693328e60d564eba7c8c5cd63c
Kyle Zsembery <k.zsembery@student.unimelb.edu.au>
2019-10-17 16:45:09 +1100

    Build

---
+++ back/build/precache-manifest.cc6655bdc6dfcd40049293f91b447d42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f316d4e1a46c24402250c9d9f23bb3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56175f31b3a690e22555",
+    "url": "/static/css/2.e71d8db5.chunk.css"
+  },
+  {
+    "revision": "13f4c0b0f8cb40ee8df2",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "56175f31b3a690e22555",
+    "url": "/static/js/2.d351765d.chunk.js"
+  },
+  {
+    "revision": "13f4c0b0f8cb40ee8df2",
+    "url": "/static/js/main.8bad8504.chunk.js"
+  },
+  {
+    "revision": "34614cd8ba68c65a4089",
+    "url": "/static/js/runtime~main.499992fe.js"
+  }
+]);
