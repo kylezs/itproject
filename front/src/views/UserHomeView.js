0956a45f75436eb87f9ac272b8d0d1a0733aff24
Zane Duffield <z.duffield@student.unimelb.edu.au>
2019-10-20 19:33:47 +1100

    missing import

--- front/src/views/UserHomeView.js
+++ front/src/views/UserHomeView.js
@@ -1,19 +1,24 @@
-import React, { useContext, useState } from 'react';
-import Layout from '../components/Layout';
-import authContext from '../authContext';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import { Typography, CssBaseline, Button, TextField, Grid,
-FormControl } from '@material-ui/core';
-import Select from '@material-ui/core/Select';
-import gql from "graphql-tag";
-import { useMutation, useQuery } from '@apollo/react-hooks';
-import ArtefactCard from '../components/ArtefactCard';
-import { Redirect } from 'react-router-dom';
-
+import React, { useContext, useState } from 'react'
+import Layout from '../components/Layout'
+import authContext from '../authContext'
+import { makeStyles } from '@material-ui/core/styles'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
+import {
+    Typography,
+    CssBaseline,
+    Button,
+    TextField,
+    Grid,
+    FormControl
+} from '@material-ui/core'
+import Select from '@material-ui/core/Select'
+import gql from 'graphql-tag'
+import { useMutation, useQuery } from '@apollo/react-hooks'
+import { ArtefactCard, Loading } from '../components'
+import { Redirect } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -153,18 +158,17 @@ function UserHomeView(props) {
         return <Loading />
     }
 
-    let selectedFamily;
-    let families;
-    let profileId;
+    let selectedFamily
+    let families
+    let profileId
     if (home_data) {
-        selectedFamily = home_data.me.profile.selectedFamily;
-        families = home_data.me.isMemberOf;
-        profileId = home_data.me.profile.id;
+        selectedFamily = home_data.me.profile.selectedFamily
+        families = home_data.me.isMemberOf
+        profileId = home_data.me.profile.id
     } else {
         // Go to logout to remove token which is normally the cause of this error
-        console.error("User data was not defined, but fetched, logging out");
+        console.error('User data was not defined, but fetched, logging out')
         return <Redirect to='/logout' />
-        
     }
 
     let artefacts = []
