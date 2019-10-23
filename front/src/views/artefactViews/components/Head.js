9ca50af58f0b5b954e4ebef94d54e40ac4d01437
Zane Duffield <z.duffield@student.unimelb.edu.au>
2019-10-23 21:39:09 +1100

    alignment fix

--- front/src/views/artefactViews/components/Head.js
+++ front/src/views/artefactViews/components/Head.js
@@ -8,54 +8,76 @@ export default withGetScreen(
         const { create, edit, view } = states.mode
         const showEditButton = view && isAdmin
         const showDeleteButton = edit && isAdmin
+        const showButton = showEditButton || showDeleteButton || create
         const marginLeftButton = isMobile() ? 18 : 'auto'
 
         return (
             <Fragment>
-                {(showEditButton || showDeleteButton || create) && !isMobile() && (
-                    <div
-                        className={classes.editLinkButton}
-                        style={{
-                            marginRight: 'auto',
-                            // width: '65px',
-                            height: '20px',
-                            visibility: 'hidden'
-                        }}
-                    />
+                {((showButton && !isMobile()) || !showButton) && (
+                    <Grid item>
+                        <div
+                            className={classes.editLinkButton}
+                            style={{
+                                marginRight: 'auto',
+                                // width: '65px',
+                                height: '20px',
+                                visibility: 'hidden'
+                            }}
+                        />
+                    </Grid>
                 )}
-                <Typography variant='h4' align='center'>
-                    {create && 'Create'} {edit && 'Edit'} {view && 'View'}{' '}
-                    Artefact
-                </Typography>
-                {showEditButton && (
-                    <Button
-                        color='secondary'
-                        variant='contained'
-                        onClick={e => setters.setMode({ edit: true })}
-                        className={classes.editLinkButton}
-                        style={{ marginLeft: marginLeftButton }}
-                    >
-                        Edit
-                    </Button>
+                <Grid item>
+                    <Typography variant='h4' align='center'>
+                        {create && 'Create'} {edit && 'Edit'} {view && 'View'}{' '}
+                        Artefact
+                    </Typography>
+                </Grid>
+                {!showButton && (
+                    <Grid item>
+                        <div
+                            className={classes.editLinkButton}
+                            style={{
+                                marginLeft: 'auto',
+                                // width: '65px',
+                                height: '20px',
+                                visibility: 'hidden'
+                            }}
+                        />
+                    </Grid>
                 )}
-                {showDeleteButton && (
-                    <Button
-                        color='default'
-                        variant='contained'
-                        onClick={openDelete}
-                        className={classes.editLinkButton}
-                        style={{ marginLeft: marginLeftButton }}
-                    >
-                        Delete
-                    </Button>
-                )}
+                {showButton && (
+                    <Grid item>
+                        {showEditButton && (
+                            <Button
+                                color='secondary'
+                                variant='contained'
+                                onClick={e => setters.setMode({ edit: true })}
+                                className={classes.editLinkButton}
+                                style={{ marginLeft: marginLeftButton }}
+                            >
+                                Edit
+                            </Button>
+                        )}
+                        {showDeleteButton && (
+                            <Button
+                                color='default'
+                                variant='contained'
+                                onClick={openDelete}
+                                className={classes.editLinkButton}
+                                style={{ marginLeft: marginLeftButton }}
+                            >
+                                Delete
+                            </Button>
+                        )}
 
-                {create && (
-                    <CreateButton
-                        noErrors={noErrors}
-                        className={classes.editLinkButton}
-                        style={{ marginLeft: marginLeftButton }}
-                    />
+                        {create && (
+                            <CreateButton
+                                noErrors={noErrors}
+                                className={classes.editLinkButton}
+                                style={{ marginLeft: marginLeftButton }}
+                            />
+                        )}
+                    </Grid>
                 )}
                 {create && (
                     <Grid item xs={12}>
