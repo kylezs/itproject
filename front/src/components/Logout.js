fa62a37dab211a30a6c66be73bf70f56557fec36
Kyle Zsembery <zsembery.kyle@gmail.com>
2019-11-15 19:12:03 +1100

    Add author statement on logout

--- front/src/components/Logout.js
+++ front/src/components/Logout.js
@@ -1,3 +1,8 @@
+/**
+ * @summary Used by route to allow logout for user
+ * @author Kyle Zsembery
+ */
+
 import React, { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 import authContext from '../authContext';
