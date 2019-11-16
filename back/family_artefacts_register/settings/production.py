7fd09e28e4bb72f3354b479e8a2ad2eb277f92db
Kyle Zsembery <zsembery.kyle@gmail.com>
2019-11-16 17:27:48 +1100

    safe settings

--- back/family_artefacts_register/settings/production.py
+++ back/family_artefacts_register/settings/production.py
@@ -8,13 +8,14 @@ We also want to use the DB that's hosted on heroku.
 Author: Kyle Zsembery, Zhuoping Miao
 """
 
-
+import os
 from .base import *
 import dj_database_url
 DEBUG = True
 
 # NEEDS TO BE STORED AS ENV VAR
-SECRET_KEY = '1y3zcu)8516zgu9*^d)6siy6+y2qys=%emm(3+#u3*=6h#$e%u'
+SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = '1y3zcu)8516zgu9*^d)6siy6+y2qys=%emm(3+#u3*=6h#$e%u'
 
 
 
@@ -32,8 +33,10 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Artefacts Picture (AWS S3)
 
-AWS_ACCESS_KEY_ID = 'AKIARVLDMTEQV22SWEWB'
-AWS_SECRET_ACCESS_KEY = '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'
+# AWS_ACCESS_KEY_ID = 'AKIARVLDMTEQV22SWEWB'
+AWS_ACCESS_KEY_ID = os.environ('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = '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'
+AWS_SECRET_ACCESS_KEY = os.environ('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'artefact-picture'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
