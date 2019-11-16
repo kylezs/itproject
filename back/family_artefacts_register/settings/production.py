be1cfcf6e4d51ffd428dd7333e4dd3a2fc03b0d3
Kyle Zsembery <zsembery.kyle@gmail.com>
2019-11-16 17:43:27 +1100

    Update secret key setting

--- back/family_artefacts_register/settings/production.py
+++ back/family_artefacts_register/settings/production.py
@@ -14,9 +14,7 @@ import dj_database_url
 DEBUG = True
 
 # NEEDS TO BE STORED AS ENV VAR
-SECRET_KEY = env('SECRET_KEY')
-# SECRET_KEY = '1y3zcu)8516zgu9*^d)6siy6+y2qys=%emm(3+#u3*=6h#$e%u'
-
+SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # Just for testing, replaced by heroku url
@@ -33,9 +31,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Artefacts Picture (AWS S3)
 
-# AWS_ACCESS_KEY_ID = 'AKIARVLDMTEQV22SWEWB'
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = 'AKIARVLDMTEQV22SWEWB'
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'artefact-picture'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
